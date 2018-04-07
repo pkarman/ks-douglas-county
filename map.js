dd0664cb74e82cdcff2aa38b6b51636d6973b55a
Peter Karman <karpet@peknet.com>
2018-04-07 15:10:19 -0500

    allow link directly to a precinct

--- map.js
+++ map.js
@@ -254,7 +254,43 @@ geojson.on('data:loaded', function() {
   $('#mask').ploading({action: 'hide'});
   $('#mask').hide();
   makePrecinctSelector();
+  // populate form from url params if present
+  var urlParams = getJsonFromUrl();
+  if (urlParams['precinct']) {
+    showPrecinct(urlParams['precinct']);
+  }
 });
+
+// https://stackoverflow.com/questions/8486099/how-do-i-parse-a-url-query-parameters-in-javascript
+function getJsonFromUrl(hashBased) {
+  var query;
+  if(hashBased) {
+    var pos = location.href.indexOf("#");
+    if(pos==-1) return [];
+    query = location.href.substr(pos+1);
+  } else {
+    query = location.search.substr(1);
+  }
+  var result = {};
+  query.split("&").forEach(function(part) {
+    if(!part) return;
+    part = part.split("+").join(" "); // replace every + with space, regexp-free version
+    var eq = part.indexOf("=");
+    var key = eq>-1 ? part.substr(0,eq) : part;
+    var val = eq>-1 ? decodeURIComponent(part.substr(eq+1)) : "";
+    var from = key.indexOf("[");
+    if(from==-1) result[decodeURIComponent(key)] = val;
+    else {
+      var to = key.indexOf("]",from);
+      var index = decodeURIComponent(key.substring(from+1,to));
+      key = decodeURIComponent(key.substring(0,from));
+      if(!result[key]) result[key] = [];
+      if(!index) result[key].push(val);
+      else result[key][index] = val;
+    }
+  });
+  return result;
+}
 
 var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
