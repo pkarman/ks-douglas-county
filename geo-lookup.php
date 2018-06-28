c06ed25a5ce41064c34259ec0ca343d6d2e9c1b7
Peter Karman <karpet@peknet.com>
2018-06-28 12:33:02 -0500

    turn off ssl name check on geo lookup (php cert issue)

--- geo-lookup.php
+++ geo-lookup.php
@@ -20,13 +20,16 @@
 
   function fetch_geocode($key, $address) {
     $base_url = 'https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?benchmark=9&format=json&address=';
-    $opts = array('http' => array(
-      'method' => 'GET',
-      'header' => "User-Agent: statedems/1.0\r\nAccept: application/json\r\n"
-      )
+    $opts = array(
+      'http' => array(
+        'method' => 'GET',
+        'header' => "User-Agent: statedems/1.0\r\nAccept: application/json\r\n",
+      ),
+      'ssl' => array('verify_peer' => false, 'verify_peer_name' => false )
     );
     $context = stream_context_create($opts);
-    $json = file_get_contents($base_url . urlencode($address), false, $context);
+    $url = $base_url . urlencode($address);
+    $json = file_get_contents($url, false, $context);
     if ($json) {
       $cache = new FileCache();
       $cache->save($key, $json, CACHE_TTL);
