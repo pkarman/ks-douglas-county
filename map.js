efe7934446f31641d21ef73132049bf3d1d956dc
Peter Karman <karpet@peknet.com>
2018-06-28 12:34:12 -0500

    Update polling places for 2018
    The page at https://www.douglascountyks.org/depts/voting-and-elections/where-to-vote
    has the best info, so parse the HTML and create the .json file from that.

--- map.js
+++ map.js
@@ -86,15 +86,7 @@ function buildPrecinct2Person() {
 };
 
 var find_polling_place = function(props) {
-  var place = '';
-  $.each(polling_places, function(idx, pp) {
-    //console.log(pp, props.precinctid);
-    if (pp.Precinct == props.precinctid) {
-      place = pp.PollingPlace;
-      return false;
-    }
-  });
-  return place;
+  return polling_places[props.precinctid];
 }
 
 var precinct_details = function(props) {
@@ -109,23 +101,23 @@ var precinct_details = function(props) {
     tbl.append('<tr><th>Committeeperson</th><td>'+cmte.Name+'</td></tr>');
   });
   var polling_place = find_polling_place(props);
-  tbl.append('<tr><th>Polling Location</th><td>'+polling_place+'</td></tr>');
+  tbl.append('<tr><th>Polling Location</th><td><a href="'+polling_place.url+'">'+polling_place.name+'</a><br/>'+polling_place.address+'</td></tr>');
   // add a marker for the polling place
-  if (!POLL_CACHE[polling_place]) {
-    $.getJSON(GEO_LOOKUP+encodeURIComponent(polling_place), function(data) {
-        if (!data.result.addressMatches || data.result.addressMatches.length == 0) return;
+  if (!POLL_CACHE[polling_place.address]) {
+    $.getJSON(GEO_LOOKUP+encodeURIComponent(polling_place.address), function(data) {
+      if (!data.result.addressMatches || data.result.addressMatches.length == 0) return;
 
-        var result = data.result.addressMatches[0];
-        var lat, lng, popstr, marker, icon;
-        popstr = 'Polling Location:<br/>' + result.matchedAddress;
-        lat = result.coordinates.y;
-        lng = result.coordinates.x;
-        icon = L.icon({
-          iconUrl:'https://cdn.vectorstock.com/i/thumb-large/88/29/ballot-box-line-icon-vector-17948829.jpg',
-          iconSize:[30, 30]
-        });
-        marker = L.marker([lat, lng], { icon: icon}).addTo(map).bindPopup(popstr);
-        POLL_CACHE[polling_place] = marker;
+      var result = data.result.addressMatches[0];
+      var lat, lng, popstr, marker, icon;
+      popstr = 'Polling Location:<br/>' + result.matchedAddress;
+      lat = result.coordinates.y;
+      lng = result.coordinates.x;
+      icon = L.icon({
+        iconUrl:'https://cdn.vectorstock.com/i/thumb-large/88/29/ballot-box-line-icon-vector-17948829.jpg',
+        iconSize:[30, 30]
+      });
+      marker = L.marker([lat, lng], { icon: icon}).addTo(map).bindPopup(popstr);
+      POLL_CACHE[polling_place.address] = marker;
     })
     .done(function(data) {
       //console.log('polling geo lookup done');
